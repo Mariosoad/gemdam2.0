df2438b1432668a6a7c32057e9ec22e453d40d77
Mario Hinostroza <mhinostroza@cfotechlatam.com>
2023-11-13 11:12:41 -0300

    lala

--- app/layout.js
+++ app/layout.js
@@ -42,8 +42,8 @@ export default function RootLayout({ children }) {
         <meta name="description" content="Nuestro equipo multidisciplinario de profesionales está enfocado en llevar a cabo soluciones tecnológicas innovadoras, que contribuyan a crear un futuro digital y cognitivo." />
         <meta name="keywords" content="Realidad Virtual, VR, virtual reality, augmented reality, Realdad Aumentada, AR, experience, nultimedia, diseño, design, simulator, environment, desarrollo web, website, 3D, react, gaming, videojuegos, archviz, arquitectura, recorrido, software" />
         <meta name="color-scheme" content="blue" />
-        <link rel="canonical" href="https://www.gemdam.com" />
-        <link rel="alternate" hreflang="es" href="https://www.gemdam.com" />
+        <link rel="canonical" href="https://gemdam.com" />
+        <link rel="alternate" hreflang="es" href="https://gemdam.com" />
         <link rel="alternate" hreflang="en" href="https://gemdam.com" />
         <meta name="robots" content="index,follow" />
         <link rel="sitemap" type="application/xml" href="https://gemdam.com/sitemap.xml" />
