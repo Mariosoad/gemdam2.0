e3813d467a443b258a2d7210a2eccc157c7f1319
Mario Hinostroza <mhinostroza@cfotechlatam.com>
2024-02-01 14:15:15 -0300

    add google analitycs

--- app/layout.js
+++ app/layout.js
@@ -41,11 +41,20 @@ export default function RootLayout({ children }) {
         <meta name="description" content="Nuestro equipo multidisciplinario de profesionales está enfocado en llevar a cabo soluciones tecnológicas innovadoras, que contribuyan a crear un futuro digital y cognitivo." />
         <meta name="keywords" content="Realidad Virtual, VR, virtual reality, augmented reality, Realdad Aumentada, AR, experience, nultimedia, diseño, design, simulator, environment, desarrollo web, website, 3D, react, gaming, videojuegos, archviz, arquitectura, recorrido, software" />
         <meta name="color-scheme" content="blue" />
-        <link rel="canonical" href="https://gemdam.com" />
-        {/* <link rel="alternate" hreflang="es" href="https://gemdam.com" />
-        <link rel="alternate" hreflang="en" href="https://gemdam.com" />
+        <link rel="canonical" href="https://www.gemdam.com" />
+        <link rel="alternate" hreflang="es" href="https://www.gemdam.com" />
+        <link rel="alternate" hreflang="en" href="https://www.gemdam.com" />
         <meta name="robots" content="index,follow" />
-        <link rel="sitemap" type="application/xml" href="https://gemdam.com/sitemap.xml" /> */}
+        <link rel="sitemap" type="application/xml" href="https://www.gemdam.com/sitemap.xml" />
+
+        {/* Google tag GEMDAM (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8XYCR02B4V" />
+        <Script id="google-analytics">
+          {` window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-8XYCR02B4V'); `}
+        </Script>
 
       </head>
       <body className={`${lexend.variable} ${roboto.variable} ${manrope.variable}`}>
