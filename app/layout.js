969323a17a1eb7ddb66e339b6144000c81b9247c
Mario Hinostroza <mariohinostroza@Marios-MacBook-Air.local>
2025-03-06 16:50:54 -0300

    fix: modified service section

--- app/layout.js
+++ app/layout.js
@@ -31,9 +31,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
       <head>
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        {/* <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
+        {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script> */}
 
         {/* METATAGS */}
         <link rel="icon" href="./logo-gemdam.png" />
