d593e5c5e167256a3f68764e5c255f6e400e1085
Mario Hinostroza <mariohinostroza@Marios-MacBook-Air.local>
2025-04-01 20:55:45 -0300

    fix: add logo gemdam test

--- app/web/Gemdam/components/models/LogoGemdam/model.jsx
+++ app/web/Gemdam/components/models/LogoGemdam/model.jsx
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
-import { useFrame } from 'react-three-fiber';
+// import { useFrame } from 'react-three/fiber';
 
 export function ModelGemdam(props) {
   const { nodes, materials } = useGLTF('/gemdamlogo.glb')
 
   const modelRef = useRef();
-  useFrame(() => {
-      if (modelRef.current) {
-        modelRef.current.rotation.y += 0.005;
-      }
-    });
+  // useFrame(() => {
+  //     if (modelRef.current) {
+  //       modelRef.current.rotation.y += 0.005;
+  //     }
+  //   });
 
   return (
         <group ref={modelRef}  scale={[4,4,4]} {...props} dispose={null}>
