cbe65e63dae2f2738a83600cd536d87c2174ffbb
Mario Hinostroza <mariohinostroza@Marios-MacBook-Air.local>
2025-04-04 01:08:48 -0300

    fix: add poster model gemdam

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Navigation from './web/Gemdam/components/navigation/navigation';
 import Gemdam from './web/Gemdam/web/gemdam/gemdam';
@@ -10,8 +10,6 @@ import Companies from './web/Gemdam/web/companies/companies';
 import About from './web/Gemdam/web/about/about';
 import Contact from './web/Gemdam/web/contact/contact';
 import Services from './web/Gemdam/web/services/services';
-// import gsap from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import Mouse from './web/Gemdam/mouse';
 
@@ -19,13 +17,9 @@ import dataEN from './language/gemdam_en.json';
 import dataES from './language/gemdam_es.json';
 
 import "./globals.css"
-// import { Canvas } from 'react-three-fiber';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Homepage() {
 
-  // gsap.registerPlugin(ScrollTrigger);
   const mainRef = useRef(null);
   const sceneRef = useRef(null);
 
@@ -36,37 +30,6 @@ export default function Homepage() {
   };
 
   const dataLanguage = currentLanguage === 'es' ? dataES : dataEN;
-
-  // useEffect(() => {
-  //   gsap.timeline({
-  //     scrollTrigger:{
-  //       trigger: mainRef.current,
-  //       start: 'top top',
-  //       end: 'bottom bottom',
-  //       scrub: 1,
-  //       // onUpdate:  ()=>
-  //       // {
-  //       //   // const scroll = ScrollTrigger.getById('main');
-  //       //   // sceneRef.current.position.y = -scroll.scroll();
-  //       // }
-  //     }
-  //   })
-  //   .to(sceneRef.current, {
-  //     ease: 'none',
-  //     x: '-50vw',
-  //     y: '140vh',
-  //   })
-  //   .to(sceneRef.current, {
-  //     ease: 'none',
-  //     x: '50vw',
-  //     y: '240vh',
-  //   })
-  //   .to(sceneRef.current, {
-  //     ease: 'none',
-  //     x: '-50vw',
-  //     y: '340vh',
-  //   })
-  // }, []);
 
   return (
     <div className='gemdamstudio'>
